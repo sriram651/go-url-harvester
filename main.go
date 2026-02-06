06290d4058541336c136f01ff5730365e7ba1c8a
Sriram Venkatesh <vsriram353@gmail.com>
2026-02-06 19:11:17 +0530

    feat: read input file and load target URLs

--- main.go
+++ main.go
@@ -4,11 +4,12 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
 	pathToInputFile := flag.String("input", "", "The text file to be input")
-	rateOfRequests := flag.Int("rate", 5, "requests per second")
+	// rateOfRequests := flag.Int("rate", 5, "requests per second")
 	pathToOutput := flag.String("output", "", "The output text")
 
 	flag.Parse()
@@ -18,8 +19,27 @@ func main() {
 		os.Exit(2)
 	}
 
-	fmt.Println("Below are the flag values")
-	fmt.Println("input: ", *pathToInputFile)
-	fmt.Println("rate: ", *rateOfRequests)
-	fmt.Println("output: ", *pathToOutput)
+	inputBytes, err := os.ReadFile(*pathToInputFile)
+
+	if err != nil {
+		fmt.Println("Error reading file:", err)
+		os.Exit(2)
+	}
+
+	lines := strings.Lines(string(inputBytes))
+
+	targetLinks := []string{}
+
+	for line := range lines {
+		trimmedLine := strings.TrimSpace(line)
+
+		if trimmedLine == "" {
+			continue
+		}
+
+		targetLinks = append(targetLinks, trimmedLine)
+		fmt.Println(trimmedLine)
+	}
+
+	fmt.Println("\nTotal target links loaded from the file:", len(targetLinks))
 }
