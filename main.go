4f894b5c76c60763fc748a54471fddbece369b7d
Sriram Venkatesh <vsriram353@gmail.com>
2026-02-07 13:02:34 +0530

    feat: implement job processing with goroutines for fetching target links

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"time"
 )
 
 func main() {
@@ -41,4 +42,24 @@ func main() {
 	}
 
 	fmt.Println("\nLoaded", len(targetLinks), "targets")
+
+	// This creates a channel - a conveyer belt where you send stuff (jobs)
+	jobs := make(chan string)
+
+	// This is the worker function that waits for the job just beside the conveyer belt
+	go func() {
+		for job := range jobs {
+			fmt.Println("fetching", job)
+			time.Sleep(2000 * time.Millisecond)
+		}
+	}()
+
+	// This is the owner function that puts the boxes (jobs) into the channel whenever the worker is ready to pick it up.
+	for _, link := range targetLinks {
+		jobs <- link
+	}
+
+	// This notifies the owner that there are no more boxes to put on the belt,
+	// so he shuts off the belt once the worker receives the last job
+	close(jobs)
 }
